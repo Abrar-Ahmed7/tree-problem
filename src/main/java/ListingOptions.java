ef622f5ded5c0abfe692c8aa7927cfd03471d8c0
abrar-ahmed7 <abrarahmedid@gmail.com>
2022-07-14 10:02:12 +0530

    implemented Stories 1,2,3,8

---
+++ src/main/java/ListingOptions.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ListingOptions {
+    public void printDirectoriesAndFiles(List<File> files, int index, int level){
+        if (index == files.size())
+            return;
+        for (int i = 0; i <= level; i++) {
+            System.out.print("\t|");
+        }
+        if (files.get(index).isFile())
+            System.out.println("--"+files.get(index).getName());
+        else if (files.get(index).isDirectory()) {
+            System.out.println("-" + files.get(index).getName());
+            printDirectoriesAndFiles(List.of(files.get(index).listFiles()),0,level + 1);
+        }
+        printDirectoriesAndFiles(files, ++index, level);
+    }
+
+    public void printDirectories(List<File> files, int index, int level){
+        if (index == files.size())
+            return;
+        for (int i = 0; i <= level; i++) {
+            System.out.print("\t|");
+        }
+        if (files.get(index).isDirectory()) {
+            System.out.println("-" + files.get(index).getName());
+            printDirectories(getDirectories(List.of(files.get(index).listFiles())),0,level + 1);
+        }
+        printDirectories(files, ++index, level);
+    }
+
+    public void printWithRelativePath(String path) {
+        try (Stream<Path> stream = Files.walk(Path.of(path))) {
+            stream.map(fileName->{
+                    String[] fileNames = fileName.toString().split("/");
+                return "\t|".repeat(Math.max(0, fileNames.length - 1)) +"-"+fileName;
+                }).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void printWithoutIndent(String path) {
+        try (Stream<Path> stream = Files.walk(Path.of(path))) {
+            stream.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<File> getDirectories(List<File> files) {
+        return files.stream().filter(File::isDirectory).collect(Collectors.toList());
+    }
+}
--- src/main/java/TreeProblem.java
+++ src/main/java/TreeProblem.java
@@ -1,5 +1,23 @@
+import java.io.File;
+import java.util.List;
+
 public class TreeProblem {
     public static void main(String[] args) {
+        ListingOptions listingOptions = new ListingOptions();
+        File directoryPath = new File("src");
+        if (directoryPath.exists() && directoryPath.isDirectory()) {
+            System.out.println(directoryPath.getName());
+            List<File> files  = List.of(directoryPath.listFiles());
+            List<File> directories = listingOptions.getDirectories(files);
+            listingOptions.printDirectoriesAndFiles(files, 0, 0);
+            System.out.println("\n-----Only Directories-----\n");
+            System.out.println(directoryPath.getName());
+            listingOptions.printDirectories(directories,0,0);
+        }
+        System.out.println("\n-------Listing with Relative Path\n");
+        listingOptions.printWithRelativePath("src");
+        System.out.println("\n-------Listing without Indentation\n");
+        listingOptions.printWithoutIndent("src");
 
     }
 }

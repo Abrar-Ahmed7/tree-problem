ab6ad4d388ba84f85578b9f68af51eb096a9b74b
abrar-ahmed7 <abrarahmedid@gmail.com>
2022-07-28 08:27:49 +0530

    refactored testcases

--- src/test/java/TreeProblemTest.java
+++ src/test/java/TreeProblemTest.java
@@ -14,13 +14,20 @@ public class TreeProblemTest {
 
     @Test
     public void itShouldReturnDirectoriesAndFiles() {
-        TreeNode treeNode = new TreeNode();
 
         //Creating Directories and Files
         createDirectories();
 
         //Given
-        List<String> expectedOutput = Arrays.asList("resources", "|--dummy", "|\t|--dummy.txt", "|\t|--dummy2.txt", "|--dummy.txt");
+        List<String> expectedOutput = Arrays.asList("resources",
+                "|--src",
+                "\t|--main",
+                "\t\t|--java",
+                "\t\t|\t|--in",
+                "\t\t|\t\t|--Grader.java",
+                "\t\t|\t\t|--Grade.java",
+                "\t\t|\t\t|--Student.java",
+                "\t\t|--resources");
 
         //When
         File file = new File("src/test/resources");
@@ -31,13 +38,11 @@ public class TreeProblemTest {
         conditions.put("viewPermission", false);
         conditions.put("xmlTree", false);
         conditions.put("jsonTree", false);
-        TreeNode<File> dirTree = treeNode.rootDirTree(file, dirConditions);
-        List<StringBuilder> resultantOutput = treeNode.getDirectoryTree(dirTree, conditions);
+        ListingOptions listingOptions = new ListingOptions();
+        List<String> resultantOutput = listingOptions.getTree(file, dirConditions, conditions);
 
         //Then
-        for (int i = 0; i < expectedOutput.size(); i++) {
-            assertEquals(expectedOutput.get(i), resultantOutput.get(i).toString());
-        }
+        assertEquals(expectedOutput, resultantOutput);
 
         //Deleting directories and files
         deleteDirectory(new File("src/test/resources"));
@@ -45,13 +50,17 @@ public class TreeProblemTest {
 
     @Test
     public void itShouldReturnDirectories() {
-        TreeNode treeNode = new TreeNode();
 
         //Creating Directories and Files
         createDirectories();
 
         //Given
-        List<String> expectedOutput = Arrays.asList("resources", "|--dummy");
+        List<String> expectedOutput = Arrays.asList("resources",
+                "|--src",
+                "\t|--main",
+                "\t\t|--java",
+                "\t\t|\t|--in",
+                "\t\t|--resources");
 
         //When
         File file = new File("src/test/resources");
@@ -62,13 +71,11 @@ public class TreeProblemTest {
         conditions.put("viewPermission", false);
         conditions.put("xmlTree", false);
         conditions.put("jsonTree", false);
-        TreeNode<File> dirTree = treeNode.rootDirTree(file, dirConditions);
-        List<StringBuilder> resultantOutput = treeNode.getDirectoryTree(dirTree, conditions);
+        ListingOptions listingOptions = new ListingOptions();
+        List<String> resultantOutput = listingOptions.getTree(file, dirConditions, conditions);
 
         //Then
-        for (int i = 0; i < expectedOutput.size(); i++) {
-            assertEquals(expectedOutput.get(i), resultantOutput.get(i).toString());
-        }
+        assertEquals(expectedOutput, resultantOutput);
 
         //Deleting directories and files
         deleteDirectory(new File("src/test/resources"));
@@ -76,13 +83,20 @@ public class TreeProblemTest {
 
     @Test
     public void itShouldReturnDirectoriesAndFilesRelativePath() {
-        TreeNode treeNode = new TreeNode();
 
         //Creating Directories and Files
         createDirectories();
 
         //Given
-        List<String> expectedOutput = Arrays.asList("src/test/resources", "|--src/test/resources/dummy", "|\t|--src/test/resources/dummy/dummy.txt", "|\t|--src/test/resources/dummy/dummy2.txt", "|--src/test/resources/dummy.txt");
+        List<String> expectedOutput = Arrays.asList("src/test/resources",
+                "|--src/test/resources/src",
+                "\t|--src/test/resources/src/main",
+                "\t\t|--src/test/resources/src/main/java",
+                "\t\t|\t|--src/test/resources/src/main/java/in",
+                "\t\t|\t\t|--src/test/resources/src/main/java/in/Grader.java",
+                "\t\t|\t\t|--src/test/resources/src/main/java/in/Grade.java",
+                "\t\t|\t\t|--src/test/resources/src/main/java/in/Student.java",
+                "\t\t|--src/test/resources/src/main/resources");
 
         //When
         File file = new File("src/test/resources");
@@ -93,13 +107,12 @@ public class TreeProblemTest {
         conditions.put("viewPermission", false);
         conditions.put("xmlTree", false);
         conditions.put("jsonTree", false);
-        TreeNode<File> dirTree = treeNode.rootDirTree(file, dirConditions);
-        List<StringBuilder> resultantOutput = treeNode.getDirectoryTree(dirTree, conditions);
+        ListingOptions listingOptions = new ListingOptions();
+        List<String> resultantOutput = listingOptions.getTree(file, dirConditions, conditions);
 
         //Then
-        for (int i = 0; i < expectedOutput.size(); i++) {
-            assertEquals(expectedOutput.get(i), resultantOutput.get(i).toString());
-        }
+        assertEquals(expectedOutput, resultantOutput);
+
 
         //Deleting directories and files
         deleteDirectory(new File("src/test/resources"));
@@ -108,16 +121,18 @@ public class TreeProblemTest {
     @Test
     public void itShouldReturnDirectoriesRelativePath() {
 
-        TreeNode treeNode = new TreeNode();
-
         //Creating Directories and Files
         createDirectories();
 
         //Given
-        List<String> expectedOutput = Arrays.asList("src/test/resources", "|--src/test/resources/dummy");
+        List<String> expectedOutput = Arrays.asList("src/test/resources",
+                "|--src/test/resources/src",
+                "\t|--src/test/resources/src/main",
+                "\t\t|--src/test/resources/src/main/java",
+                "\t\t|\t|--src/test/resources/src/main/java/in",
+                "\t\t|--src/test/resources/src/main/resources");
 
         //When
-
         File file = new File("src/test/resources");
         DirConditions dirConditions = new DirConditions();
         Map<String, Boolean> conditions = new HashMap<>();
@@ -126,13 +141,11 @@ public class TreeProblemTest {
         conditions.put("viewPermission", false);
         conditions.put("xmlTree", false);
         conditions.put("jsonTree", false);
-        TreeNode<File> dirTree = treeNode.rootDirTree(file, dirConditions);
-        List<StringBuilder> resultantOutput = treeNode.getDirectoryTree(dirTree, conditions);
+        ListingOptions listingOptions = new ListingOptions();
+        List<String> resultantOutput = listingOptions.getTree(file, dirConditions, conditions);
 
         //Then
-        for (int i = 0; i < expectedOutput.size(); i++) {
-            assertEquals(expectedOutput.get(i), resultantOutput.get(i).toString());
-        }
+        assertEquals(expectedOutput, resultantOutput);
 
         //Deleting directories and files
         deleteDirectory(new File("src/test/resources"));
@@ -140,13 +153,20 @@ public class TreeProblemTest {
 
     @Test
     public void itShouldReturnPermissionsOfDirectoriesAndFiles() {
-        TreeNode treeNode = new TreeNode();
 
         //Creating Directories and Files
         createDirectories();
 
         //Given
-        List<String> expectedOutput = Arrays.asList("[drwx]resources", "|--[drwx]dummy", "|\t|--[-rw--]dummy.txt", "|\t|--[-rw--]dummy2.txt", "|--[-rw--]dummy.txt");
+        List<String> expectedOutput = Arrays.asList("[drwx]resources",
+                "|--[drwx]src",
+                "\t|--[drwx]main",
+                "\t\t|--[drwx]java",
+                "\t\t|\t|--[drwx]in",
+                "\t\t|\t\t|--[-rw--]Grader.java",
+                "\t\t|\t\t|--[-rw--]Grade.java",
+                "\t\t|\t\t|--[-rw--]Student.java",
+                "\t\t|--[drwx]resources");
 
         //When
         File file = new File("src/test/resources");
@@ -157,13 +177,11 @@ public class TreeProblemTest {
         conditions.put("viewPermission", true);
         conditions.put("xmlTree", false);
         conditions.put("jsonTree", false);
-        TreeNode<File> dirTree = treeNode.rootDirTree(file, dirConditions);
-        List<StringBuilder> resultantOutput = treeNode.getDirectoryTree(dirTree, conditions);
+        ListingOptions listingOptions = new ListingOptions();
+        List<String> resultantOutput = listingOptions.getTree(file, dirConditions, conditions);
 
         //Then
-        for (int i = 0; i < expectedOutput.size(); i++) {
-            assertEquals(expectedOutput.get(i), resultantOutput.get(i).toString());
-        }
+        assertEquals(expectedOutput, resultantOutput);
 
         //Deleting directories and files
         deleteDirectory(new File("src/test/resources"));
@@ -177,33 +195,52 @@ public class TreeProblemTest {
         createDirectories();
 
         //Given
-        List<String> expectedOutput = Arrays.asList("<directory name=\"resources\">", "\t<directory name=\"dummy\">", "\t\t<file name=\"dummy.txt\"></file>", "\t\t<file name=\"dummy2.txt\"></file>", "\t</directory>", "\t<file name=\"dummy.txt\"></file>", "</directory>");
+        List<String> expectedOutput = Arrays.asList("<directory name=\"resources\">",
+                "\t<directory name=\"src\">",
+                "\t\t<directory name=\"main\">",
+                "\t\t\t<directory name=\"java\">",
+                "\t\t\t\t<directory name=\"in\">",
+                "\t\t\t\t\t<file name=\"Grader.java\"></file>",
+                "\t\t\t\t\t<file name=\"Grade.java\"></file>",
+                "\t\t\t\t\t<file name=\"Student.java\"></file>",
+                "\t\t\t\t</directory>",
+                "\t\t\t</directory>",
+                "\t\t\t<directory name=\"resources\">",
+                "\t\t\t</directory>",
+                "\t\t</directory>",
+                "\t</directory>",
+                "</directory>");
 
         //When
         File file = new File("src/test/resources");
         DirConditions dirConditions = new DirConditions();
         dirConditions.setOnlyDir(false);
         TreeNode<File> dirTree = treeNode.rootDirTree(file, dirConditions);
-        List<StringBuilder> resultantOutput = treeNode.getXmlDirTree(dirTree);
+        List<StringBuilder> xmlTree = treeNode.getXmlDirTree(dirTree);
+        ListingOptions listingOptions = new ListingOptions();
+        List<String> resultantOutput = listingOptions.getTreeString(xmlTree);
 
         //Then
-        for (int i = 0; i < expectedOutput.size(); i++) {
-            assertEquals(expectedOutput.get(i), resultantOutput.get(i).toString());
-        }
+        assertEquals(expectedOutput, resultantOutput);
 
         //Deleting directories and files
         deleteDirectory(new File("src/test/resources"));
     }
 
     public void createDirectories() {
-        File theDir = new File(String.valueOf(Path.of("src", "test", "resources", "dummy")));
-        if (!theDir.exists()) {
+        File theDir = new File(String.valueOf(Path.of("src", "test", "resources", "src","main","java","in")));
+        File theDir2 = new File(String.valueOf(Path.of("src", "test", "resources", "src","main","resources")));
+        if (!theDir.exists()){
             theDir.mkdirs();
         }
+        if (!theDir2.exists()){
+            theDir2.mkdirs();
+        }
+
         try {
-            File newFile = new File("src/test/resources/dummy.txt");
-            File newFile1 = new File("src/test/resources/dummy/dummy.txt");
-            File newFile2 = new File("src/test/resources/dummy/dummy2.txt");
+            File newFile = new File("src/test/resources/src/main/java/in/Grade.java");
+            File newFile1 = new File("src/test/resources/src/main/java/in/Grader.java");
+            File newFile2 = new File("src/test/resources/src/main/java/in/Student.java");
             newFile.createNewFile();
             newFile1.createNewFile();
             newFile2.createNewFile();
@@ -213,11 +250,11 @@ public class TreeProblemTest {
     }
 
     public void deleteDirectory(File file) {
-        for (File subfile : file.listFiles()) {
-            if (subfile.isDirectory()) {
-                deleteDirectory(subfile);
+        for (File subFile : file.listFiles()) {
+            if (subFile.isDirectory()) {
+                deleteDirectory(subFile);
             }
-            subfile.delete();
+            subFile.delete();
         }
     }
 }
